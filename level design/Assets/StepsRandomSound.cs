1f8c8453b02ceb96c26e8bc60258b594efec9064
mariano <marianoa.munguia@gmail.com>
2021-05-06 23:29:16 -0300

    step & shoot sound + script

---
+++ level design/Assets/StepsRandomSound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepsRandomSound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] clips;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void step()
+    {
+        AudioClip clip = GetRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+}

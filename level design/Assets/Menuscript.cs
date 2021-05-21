1f505b9fe8a760bde1a88a09459293e3bd8bd94c
mariano <marianoa.munguia@gmail.com>
2021-05-20 23:47:43 -0300

    mini menu + exit button

---
+++ level design/Assets/Menuscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menuscript : MonoBehaviour
+{
+    public GameObject menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) menu.SetActive(true);
+    }
+    public void ClickInBoton(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}

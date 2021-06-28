4cd7cffafd0e5c8ceda12da7c552ac536b005388
gonzalo.morenop <gonzalo.morenop@davinci.edu.ar>
2021-06-28 17:15:31 -0300

    fix enemy b

--- level design/Assets/IA/scripts/ia Astart/MoveToPlayer.cs
+++ level design/Assets/IA/scripts/ia Astart/MoveToPlayer.cs
@@ -6,14 +6,21 @@ public class MoveToPlayer : MonoBehaviour
 {
     public float speed=10f;
     public Pathfinding Pf;
+    public Rigidbody rb;
     // Update is called once per frame
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
     void Update()
     {
 
         float step = speed * Time.deltaTime; 
-        try { 
+        try {
             transform.position = Vector3.MoveTowards(transform.position, Pf.EnemyPath[1].vPosition, step);
-            transform.LookAt(Pf.EnemyPath[1].vPosition);
+        
+        
         }
         catch
         {
